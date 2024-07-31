40cc692c805303ef5516e49de03f3900582f2bba
Omega-Ariston <654815312@qq.com>
2024-07-31 23:32:25 +0800

    优化金币砖

--- scripts/CoinBumped.gd
+++ scripts/CoinBumped.gd
@@ -1,7 +1,7 @@
 class_name CoinBumped
 extends CharacterBody2D
 
-const BOUNCE_VELOCITY := -240.0
+const BOUNCE_VELOCITY := -300.0
 
 @onready var timer: Timer = $Timer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -12,7 +12,7 @@ func _ready() -> void:
 	timer.start()
 	
 func _physics_process(delta: float) -> void:
-	velocity.y += GameManager.default_gravity / 1.5 * delta
+	velocity.y += GameManager.default_gravity  * delta
 	move_and_slide()
 
 func _on_timer_timeout() -> void:
