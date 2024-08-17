43ae83bbac5f6d284232aee3c94dd6299df22134
Omega-Ariston <654815312@qq.com>
2024-08-18 04:52:07 +0800

    加入拉旗得分逻辑

--- scripts/FlagPole.gd
+++ scripts/FlagPole.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 @onready var flag: Sprite2D = $Flag
 @onready var climable: Climable = $Climable
+@onready var score_label: RichTextLabel = $Score
 
 const DOWN_SPEED := 130.0
 
@@ -14,24 +15,6 @@ var player: Player
 var player_destination_y: float
 var is_ended := false
 
-func _on_climable_body_entered(body: Node2D) -> void:
-	if body is Player:
-		player = body
-		SoundManager.pause_bgm()
-		GameManager.game_timer.stop()
-		SoundManager.play_sfx("FlagPole")
-		# 降落旗子
-		var flag_tween := create_tween()
-		var flag_destination_y = global_position.y - Variables.TILE_SIZE.y * 1.5
-		var flag_duration := abs(flag_destination_y - flag.global_position.y) / DOWN_SPEED as float
-		flag_tween.tween_property(flag, "global_position:y", flag_destination_y, flag_duration)
-		flag_tween.finished.connect(_on_flag_down_finished)
-		# 控制玩家下降
-		body.controllable = false
-		player_destination_y = global_position.y - Variables.TILE_SIZE.y
-		# 开始角色的滑行
-		player_slipping = true
-		
 
 func _physics_process(_delta: float) -> void:
 	if not is_ended:
@@ -49,6 +32,47 @@ func _physics_process(_delta: float) -> void:
 				player.visible = false
 				is_ended = true
 				GameManager.end_level_by_flag_pole(player, next_level)
+				
+
+func _on_climable_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player = body
+		SoundManager.pause_bgm()
+		GameManager.game_timer.stop()
+		SoundManager.play_sfx("FlagPole")
+		# 根据玩家触碰旗杆的位置决定得分
+		var score := _get_flag_score(body.global_position.y)
+		score_label.text = str(score)
+		ScoreManager.add_score(score, null, false)
+		# 降落旗子同时上升得分
+		var flag_tween := create_tween()
+		var flag_destination_y = global_position.y - Variables.TILE_SIZE.y * 1.5
+		var flag_duration := abs(flag_destination_y - flag.global_position.y) / DOWN_SPEED as float
+		flag_tween.tween_property(flag, "global_position:y", flag_destination_y, flag_duration)
+		flag_tween.finished.connect(_on_flag_down_finished)
+		var score_tween := create_tween()
+		var score_destination_y = global_position.y - Variables.TILE_SIZE.y * 10
+		var score_duration = flag_duration
+		score_tween.tween_property(score_label, "global_position:y", score_destination_y, score_duration)
+		# 控制玩家下降
+		body.controllable = false
+		player_destination_y = global_position.y - Variables.TILE_SIZE.y
+		# 开始角色的滑行
+		player_slipping = true
+
+
+func _get_flag_score(pos_y: float) -> int:
+	if pos_y <= global_position.y - Variables.TILE_SIZE.y * 9:
+		return 5000
+	elif pos_y <= global_position.y - Variables.TILE_SIZE.y * 6:
+		return 2000
+	elif pos_y <= global_position.y - Variables.TILE_SIZE.y * 5:
+		return 800
+	elif pos_y <= global_position.y - Variables.TILE_SIZE.y * 2:
+		return 400
+	else:
+		return 100
+
 
 func _on_flag_down_finished() -> void:
 	# 旗子落完角色就要停了
