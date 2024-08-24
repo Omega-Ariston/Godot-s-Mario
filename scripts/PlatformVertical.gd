6716d8a6cbf4cad1597f4025693cb1f3ed14f6e6
Omega-Ariston <654815312@qq.com>
2024-08-24 22:26:21 +0800

    添加了垂直移动平台

---
+++ scripts/PlatformVertical.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+@export var direction := -1 # -1表示向上，1表示向下
+
+const SPEED := 48
+const UP_BOUND := 0
+const DOWN_BOUND := Variables.TILE_SIZE.y * 16
+
+func _physics_process(delta: float) -> void:
+	global_position.y += direction * SPEED * delta
+	match direction:
+		-1:
+			if global_position.y < UP_BOUND:
+				global_position.y = DOWN_BOUND
+		1:
+			if global_position.y > DOWN_BOUND:
+				global_position.y = UP_BOUND
