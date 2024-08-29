a2638ea91c69294d0713e4c412a1c97fcec47736
Omega-Ariston <jiechuan.chen@pixocial.com>
2024-08-29 18:30:43 +0800

    将钻地动画通用化

--- scripts/IntoTheUnder.gd
+++ scripts/IntoTheUnder.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@export var next_level : String
-
 @onready var spawn_point: SpawnPoint = $SpawnPoint
 @onready var player: Player = $Player
 @onready var foreground: TileMapLayer = $Foreground
@@ -15,4 +13,4 @@ func _ready() -> void:
 	var tween = create_tween()
 	tween.tween_property(player, "global_position:x", player.global_position.x + Variables.TILE_SIZE.x * 7.5, 3)
 	await bgm_player.finished
-	GameManager.change_scene(next_level)
+	GameManager.change_scene(StatusBar.level)
