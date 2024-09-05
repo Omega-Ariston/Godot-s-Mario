cddb632b1fe772c349b374c30c1eb718b34e3eba
Omega-Ariston <jiechuan.chen@pixocial.com>
2024-09-05 19:24:17 +0800

    实现了火条

---
+++ scripts/FireBar.gd
@@ -0,0 +1,27 @@
+class_name FireBar
+extends Node2D
+
+const SPEED := 0.25
+
+@export var direction: Direction
+
+@onready var graphics: Node2D = $Graphics
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+enum Direction {
+	CLOCKWISE,
+	COUNTER_CLOCKWISE,
+}
+
+func _ready() -> void:
+	var rng := RandomNumberGenerator.new()
+	var current_frame := rng.randf_range(0, 0.7)
+	animation_player.speed_scale = SPEED
+	
+	match direction:
+		Direction.CLOCKWISE:
+			animation_player.play("spin")
+		Direction.COUNTER_CLOCKWISE:
+			animation_player.play_backwards("spin")
+		
+	animation_player.seek(current_frame, true)
