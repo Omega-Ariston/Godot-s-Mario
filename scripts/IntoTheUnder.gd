7dbad6969d511632cb5c4b05eeecfe0a5013c66b
Omega-Ariston <654815312@qq.com>
2024-09-14 21:10:41 +0800

    修复钻地动画的角色图层

--- scripts/IntoTheUnder.gd
+++ scripts/IntoTheUnder.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var spawn_point: SpawnPoint = $SpawnPoint
 @onready var player: Player = $Player
 @onready var foreground: TileMapLayer = $TileMap/Foreground
+@onready var marker_2d: Marker2D = $Marker2D
 
 
 func _ready() -> void:
@@ -14,3 +15,8 @@ func _ready() -> void:
 	tween.tween_property(player, "global_position:x", player.global_position.x + Variables.TILE_SIZE.x * 7.5, 3)
 	await bgm_player.finished
 	GameManager.change_scene(StatusBar.level)
+
+func _physics_process(_delta: float) -> void:
+	if player.global_position.x >= marker_2d.global_position.x:
+		# 进管子了
+		player.z_index = -1
