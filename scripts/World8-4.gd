e993228653276ba76b4de65c72e60e9c54283871
Omega-Ariston <654815312@qq.com>
2024-09-16 13:42:52 +0800

    优化8-4的传送实现，修复穿帮的部分

--- scripts/World8-4.gd
+++ scripts/World8-4.gd
@@ -12,6 +12,12 @@ extends World
 @onready var enemies_b: Node2D = $Enemies/B
 @onready var enemies_d: Node2D = $Enemies/D
 
+@onready var special_pipes: Node2D = $Enemies/SpecialPipes
+@onready var special_turtles: Node2D = $Enemies/B/SpecialTurtles
+
+const SPECIAL_PIPE_E1_POSITION := [3136, 4160]
+const SPECIAL_PIPE_E2_POSITION := [3280, 4304]
+
 const CAPTURE_RANGE := Variables.TILE_SIZE.x * 8
 
 var enemies_a_copy : Node2D
@@ -27,25 +33,24 @@ func _ready() -> void:
 	enemies_d_copy = enemies_d.duplicate()
 
 func _physics_process(_delta: float) -> void:
-	if b_enabled and detect_portal('b', 'a'):
+	if b_enabled and detect_portal('b'):
+		teleportPlayerAndRespawnEnemies('b', 'a')
 		return
-	if detect_portal('c', 'b'):
+	if detect_portal('c'):
+		# 禁用B传回A的机制
 		b_enabled = false
+		var new_enemies := teleportPlayerAndRespawnEnemies('c', 'b')
+		# 把两只乌龟也传过去
+		teleport_special_turtles()
+		# 更新两只乌龟的指针
+		special_turtles = new_enemies.get_node('SpecialTurtles')
 		return
-	if detect_portal('e', 'd'):
+	if detect_portal('e'):
+		teleportPlayerAndRespawnEnemies('e', 'd')
 		return
 
-func detect_portal(from: String, to: String) -> bool:
-	if player.global_position.x > self[from].global_position.x and player.global_position.x - self[from].global_position.x < CAPTURE_RANGE:
-		camera_2d.limit_left = 0
-		player.global_position.x = self[to].global_position.x + (player.global_position.x - self[from].global_position.x)
-		var new_enemies := self['enemies_' + to + '_copy'].duplicate() as Node2D
-		enemies.add_child(new_enemies)
-		# 手动调用敌人们的world_ready和player_ready方法
-		get_enemies_ready(new_enemies)
-		return true
-	else:
-		return false
+func detect_portal(from: String) -> bool:
+	return player.global_position.x > self[from].global_position.x and player.global_position.x - self[from].global_position.x < CAPTURE_RANGE
 
 func get_enemies_ready(node: Node2D) -> void:
 	if node is Enemy:
@@ -53,4 +58,28 @@ func get_enemies_ready(node: Node2D) -> void:
 		node._on_player_ready()
 	elif node.get_child_count() > 0:
 		for child in node.get_children():
-			get_enemies_ready(child)
+			if child is Node2D:
+				get_enemies_ready(child)
+
+func teleportPlayerAndRespawnEnemies(from: String, to: String) -> Node2D:
+	# 传送主角
+	camera_2d.limit_left = 0
+	player.global_position.x = self[to].global_position.x + (player.global_position.x - self[from].global_position.x)
+	# 传送火球
+	var fire_balls := get_tree().get_nodes_in_group("Fireballs")
+	for fire_ball : Fireball in fire_balls:
+		fire_ball.global_position.x = self[to].global_position.x + (fire_ball.global_position.x - self[from].global_position.x)
+	# 更新敌人
+	var new_enemies := self['enemies_' + to + '_copy'].duplicate() as Node2D
+	enemies.add_child(new_enemies)
+	# 手动调用敌人们的world_ready和player_ready方法
+	get_enemies_ready(new_enemies)
+	return new_enemies
+	
+
+func _on_special_pipe_screen_exited() -> void:
+	# 将两个特殊管道传送到新地点
+	special_pipes.global_position = e.global_position if special_pipes.global_position == d.global_position else d.global_position
+
+func teleport_special_turtles() -> void:
+	special_turtles.global_position.x = b.global_position.x - (c.global_position.x - special_turtles.global_position.x)
